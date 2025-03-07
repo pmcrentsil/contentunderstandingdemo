95ba5d85051e4651ec96cac29a2e3ce11f1d71f5
Rashed Talukder <9218468+rashedtalukder@users.noreply.github.com>
2025-03-06 19:13:26 -0800

    Add analyzer before running field extraction

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ import json
 endpoint = "https://contentservices.services.ai.azure.com/"  # Keep this as your Content Understanding endpoint
 subscription_key = "DfO8IZa5iAgpOg2MBWILdeK73c2zRJLiObuneHIlaUu9gxoDUisdJQQJ99BCACYeBjFXJ3w3AAAAACOG8ozL"  # Replace with your actual subscription key
 
+analyzer_id="recipe_analyzer"  # The ID of the analyzer to create
+
 # The URL for the doc intell API call (document URL endpoint)
-url = endpoint + "formrecognizer/documentModels/prebuilt-invoice/analyze?api-version=2023-02-28-preview"  # Correct API path for Form Recognizer
+url = endpoint + "contentunderstanding/analyzers/{analyzerId}?api-version=2024-12-01-preview"  # Correct API path for Content Understanding
 
 # Example document URL or file data (update with your file or URL)
 headers = {
@@ -15,13 +17,27 @@ headers = {
     'Ocp-Apim-Subscription-Key': subscription_key
 }
 
+# Load the request body from a JSON file
+with open('analyzer_template.json', 'r') as file:
+    data = json.load(file)
+
+# Make the PUT request to the Content Understanding API
+response = requests.put(url, headers=headers, json=data)
+
+if response.status_code == 201:
+    print("Created Content Understanding analyzer successfully.")
+    print("Operation-Location: ", response.headers["Operation-Location"])
+else:
+    print(f"Failed to create Content Understanding analyzer. Status code: {response.status_code}")
+    print(response.text)
+
 # Document URL (replace with the actual document URL or use a SAS URL if needed)
-data = {
-   "urlSource": "https://cooking.blob.core.windows.net/cooking/cooking.pdf"  # Replace with your document URL
+recipe_file = {
+   "url": "https://cooking.blob.core.windows.net/cooking/cooking.pdf"  # Replace with your document URL
 }
 
 # Make the POST request to the Form Recognizer API
-response = requests.post(url, headers=headers, json=data)
+response = requests.post(url, headers=headers, json=recipe_file)
 
 # Check if the request was successful
 if response.status_code == 202:
